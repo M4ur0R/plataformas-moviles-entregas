5c107e8737c9791fab9677ad5949c837222e471c
Mauro Romero <163943609+M4ur0R@users.noreply.github.com>
2024-05-28 21:26:37 -0300

    Add files via upload

---
+++ tp-7/script.js
@@ -0,0 +1,33 @@
+// // Variables
+// let variable1 = 1;
+// var variable2 = 1;
+// const variable3 = 1;
+
+// // texto
+// let string = "xd"
+// // let string = 'xd'
+// // let string = `xd` 
+
+// // condicional
+// if(condicion){
+//     //si se cumple12
+// } else {
+//     //si no...
+// }
+
+// while(condicion){
+
+// }
+
+// for(inicio; condicion; incremento){
+
+// }
+// do{} while(condicion)
+// switch(valor){
+//     case 1:
+//         break;
+//     case 2:
+//         break;
+//     default:
+//         break;
+// }
